b5fb583931f44df7da088d1c69707df1b050fa25
Tahfimul <victory.josh.josh24@gmail.com>
2018-05-06 18:19:48 -0400

    continued adding routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,4 +1,4 @@
-//Handler for /products get request
+//Handler for /orders get request
 
 //import express package
 const express = require('express');
@@ -7,16 +7,14 @@ const express = require('express');
 const router = express.Router();
 
 //Register different routes using router package
-//middleware function for handling incoming request
+//middleware function for handling incoming get request
 //req is to get request
 //res is to get response
 //next is a function which moves the request to next middleware in line
 router.get('/', (req, res, next)=>{
-  //send json response by setting a status code 200
- res.status(200).json({
-     //pass javascript object
-     message: 'Handling GET requests to /products'
- });
+  res.status(201).json({
+    message: 'Order was fetched'
+  });
 });
 
 //Register different routes using router package
@@ -25,57 +23,34 @@ router.get('/', (req, res, next)=>{
 //res is to get response
 //next is a function which moves the request to next middleware in line
 router.post('/', (req, res, next)=>{
-  //send json response by setting a status code 200
- res.status(200).json({
-     //pass javascript object
-     message: 'Handling POST requests to /products'
- });
-});
-
-//Register different routes using router package
-//middleware function for handling incoming get request to /productID
-//req is to get request
-//res is to get response
-//next is a function which moves the request to next middleware in line
-router.get('/:id', (req, res, next)=>{
-  const id = req.params.id;
-  if (id === 'special') {
-    //send json response by setting a status code 201
-    res.status(201).json({
-       //pass javascript object
-      message: 'You discovered the special ID',
-      id: id
-    });
-  }
-  else{
-    res.status(201).json({
-      message: 'You just passed an ID',
-      id: id
-    });
-  }
-});
-
-//Register different routes using router package
-//middleware function for handling incoming patch request to /productID
-//req is to get request
-//res is to get response
-//next is a function which moves the request to next middleware in line
-router.patch('/:id', (req, res, next)=>{
   res.status(201).json({
-    message: 'Updated Product!'
+    message: 'Order Created'
   });
 });
 
 //Register different routes using router package
-//middleware function for handling incoming delete request to /productID
+//middleware function for handling incoming get request to /id
+//req is to get request
+//res is to get response
+//next is a function which moves the request to next middleware in line
+router.get('/:id', (req, res, next)=>{
+  res.status(201).json({
+    message: 'Order details',
+    order: req.params.id
+  });
+});
+
+//Register different routes using router package
+//middleware function for handling incoming delete request to /id
 //req is to get request
 //res is to get response
 //next is a function which moves the request to next middleware in line
 router.delete('/:id', (req, res, next)=>{
   res.status(201).json({
-    message: 'Prodct Deleted!'
-  });
-});
+    message: 'Order deleted',
+    order: req.params.id
+  })
+})
 
 //set router as a module using exports which converts router to module so that it can be used in other files
 //module is a variable which represents current module
--- app.js
+++ app.js
@@ -7,10 +7,17 @@ const app = express();
 
 //import products.js
 const productRoutes = require('./api/routes/products');
+//import orders.js
+const orderRoutes = require('./api/routes/orders');
 
 //middleware function for handling incoming request
 //only requests that contain "/products" will be handled using products.js handler
 app.use('/products',productRoutes);
+
+//middleware function for handling incoming request
+//only requests that contain "/orders" will be handled using orders.js handler
+app.use('/orders', orderRoutes);
+
 //set app as a module using exports which converts app to module
 //module is a variable which represents current module
 module.exports = app;
